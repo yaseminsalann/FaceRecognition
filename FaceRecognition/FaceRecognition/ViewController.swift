fa9316abe7dbf5114790c879b695ec6859ccab5b
yaseminsalann <yaseminsalann@gmail.com>
2025-02-10 03:21:58 +0300

    yüz tanıma
    yüz tanıma projesi

---
+++ FaceRecognition/FaceRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceRecognition
+//
+//  Created by Yasemin salan on 10.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FaceRecognition/FaceRecognitionTests/FaceRecognitionTests.swift
@@ -0,0 +1,17 @@
+//
+//  FaceRecognitionTests.swift
+//  FaceRecognitionTests
+//
+//  Created by Yasemin salan on 10.02.2025.
+//
+
+import Testing
+@testable import FaceRecognition
+
+struct FaceRecognitionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
